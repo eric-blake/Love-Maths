edb828bdec1b9faacb602cf5f4ca645d97428d66
eric-blake <>
2023-05-26 18:00:25 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,8 @@ document.addEventListener("DOMContentLoaded", function () { //when the page is f
         //go through buttons array and return each element which will be stored in the variable"button" on each itertion
         button.addEventListener("click", function () { // add event listner to buttons (listenes for button to be clicked, and when clicked the code in this block will be executed)
             if (this.getAttribute("data-type") === "submit") { // "this" refers to the button that was just clicked
-                alert("You clicked Submit!"); //check the attribute of the data type to see what its value is, if its submit we are going to display an alert
+                checkAnswer();
+                // alert("You clicked Submit!"); //check the attribute of the data type to see what its value is, if its submit we are going to display an alert
                 //We then use the reference to  that button to call its get attribute method and check the contents  of our data type custom attribute. 
                 //If it is equal to submit then we're going to display  an alert to tell the user, "you clicked submit". Otherwise, we're going to set the game  
                 //type to the value of that attribute.  And this will tell us what  game type we're wanting to run.
@@ -40,9 +41,8 @@ function runGame(gameType) {  //need to supply the parameters that the function 
     let num2=Math.floor(Math.random() * 25) + 1;
     
 
-    //What we just need to do is check which game  type we're running with an if statement.
+    //What we just need to do is check which game type we're running with an if statement.
     //And call the appropriate function to display  the question. So let's put that in here,  
-
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
@@ -53,16 +53,55 @@ function runGame(gameType) {  //need to supply the parameters that the function 
 
 }
 
-
-
-
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
+    let userAnswer =parseInt(document.getElementById("answer-box").value)
+    //ecause it's an input element we need to get the value from it. We can't use inner text for this.
+    let calculatedAnswer = calculateCorrectAnswer(); //calculatecorrectAnswer returns an array of return[operand1 + operand2, "addition"]; 
+    let isCorrect = userAnswer === calculatedAnswer[0]; //returns the first element in the array
+
+    if (isCorrect) {
+        alert("Hey you got it right");
+            } 
+            else {
+                    alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+            }
+
+          
+            runGame(calculatedAnswer[1]);
+}
+
+
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
+function calculateCorrectAnswer() {
+    //we're using the parse int function to make sure that we treat the  value as an integer, a whole number.
+    //By default, when JavaScript gets data from the dom it returns it as a string but as you know we  can't do mathematical operations on a string  
+
+let operand1 = parseInt(document.getElementById('operand1').innerText);
+let operand2 = parseInt(document.getElementById('operand2').innerText);
+let operator = document.getElementById("operator").innerText;
+//we want to calculate the  correct answer based on the game type and we're determining the game type by the  operator. So let's put an if statement in  
+//here and we're saying if the operator is equal  to the plus sign, it must be the addition game, and so to return our correct answer  operand 1 plus operand 2 and the game type.  
+if (operator ==="+"){
+    return[operand1 + operand2, "addition"];
+}
+
+//f we don't recognize our  operator then we'll alert the user. sAnd we'll also throw an error.  
+    else {
+        alert(`Unimplemented operator ${operator}`)
+        throw`Unimplemented operator ${operator}.aborting;`
+    }
 
 }
 
-function calculatecorrectAnswer() {
 
-}
+
 
 function incermentScore() {
 
