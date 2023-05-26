b8cc235fc20043bf98d60255f0a00db6bee555cf
eric-blake <>
2023-05-26 16:16:21 +0000

    Fix JS link in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 // Wait for the DOm to fininh loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() { //when the page is finished loading, the code within the code block will execute
+document.addEventListener("DOMContentLoaded", function () { //when the page is finished loading, the code within the code block will execute
     let buttons = document.getElementsByTagName("button");  //returns all the buttons on the page as an array, and we can iterate throught the array
 
     for (let button of buttons) {  //more modern syntax of for(let i=0; i< buttons.length; i++)
         //go through buttons array and return each element which will be stored in the variable"button" on each itertion
-        button.addEventListener("click", function(){ // add event listner to buttons (listenes for button to be clicked, and when clicked the code in this block will be executed)
-            if(this.getAttribute("data-type")==="submit"){ // "this" refers to the button that was just clicked
+        button.addEventListener("click", function () { // add event listner to buttons (listenes for button to be clicked, and when clicked the code in this block will be executed)
+            if (this.getAttribute("data-type") === "submit") { // "this" refers to the button that was just clicked
                 alert("You clicked Submit!"); //check the attribute of the data type to see what its value is, if its submit we are going to display an alert
                 //We then use the reference to  that button to call its get attribute method and check the contents  of our data type custom attribute. 
                 //If it is equal to submit then we're going to display  an alert to tell the user, "you clicked submit". Otherwise, we're going to set the game  
@@ -18,11 +18,11 @@ document.addEventListener("DOMContentLoaded", function() { //when the page is fi
                 //So we're going to go run game and then  the parameter is going to be the game type.
                 runGame(gameType);
             }
-         });
+        });
     }
 
-   runGame("addition"); //The first thing is that we want an addition  game to start as soon as the page is loaded.  
-   //It's going to be our default game. So we need to  add that to our dom content loaded event listener.So inside that event listener  but outside of the for loop,  
+    runGame("addition"); //The first thing is that we want an addition  game to start as soon as the page is loaded.  
+    //It's going to be our default game. So we need to  add that to our dom content loaded event listener.So inside that event listener  but outside of the for loop,  
 });
 
 
@@ -36,20 +36,19 @@ function runGame(gameType) {  //need to supply the parameters that the function 
 
 
     //creates 2 random numbers between 1 & 25
-//let num1=Math.floor(Math.random() * 25) + 1;
-//let num2=Math.floor(Math.random() * 25) + 1;
-let num1=5;
-let num2=6;
+    let num1=Math.floor(Math.random() * 25) + 1;
+    let num2=Math.floor(Math.random() * 25) + 1;
+    
 
-//What we just need to do is check which game  type we're running with an if statement.
-//And call the appropriate function to display  the question. So let's put that in here,  
+    //What we just need to do is check which game  type we're running with an if statement.
+    //And call the appropriate function to display  the question. So let's put that in here,  
 
-if (gameType === "addition") {
-    displayAdditionQuestion(num1, num2);
-} else {
-    alert(`Unknown game type: ${gameType}`);
-    throw `Unknown game type: ${gameType}. Aborting!`;
-}
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 
 }
@@ -80,13 +79,15 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
 
+
 }
 
 function dispalaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion()
+ {
 
 }
 
